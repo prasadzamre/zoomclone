7036b8a757f51ea059fa77a836bfa0d0ecf01e7b
prasad Zamre <prasadza1@icloud.com>
2021-10-05 06:33:52 +0530

    Working on Menu Buttons

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { View, Text ,StyleSheet} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 export default function SearchBar() {
-    return (
-        <View>
-            <Text>Hey I am Search bar</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Fontisto name="search" size={20} color={"#858585"} />
+      <Text style={styles.title}>Search</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-    
-}
-})
+  container: {
+    backgroundColor: "#333333",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    height: 40,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  title: {
+    color: "#858585",
+    paddingLeft: 10,
+    fontSize: 20,
+  },
+});
